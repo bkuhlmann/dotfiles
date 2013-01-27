74f0ee0341e598083f1f5e96eed6d66eb9edfc6f
Brooke Kuhlmann <brooke@redalchemist.com>
2013-01-27 16:42:09 -0700

    Converted Ruby install script to Bash script.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "\nInstalling dotfiles...\n"
+
+if [ -d "$HOME" ]; then
+  for file in `ls -1 home_files`
+  do
+    source_file="home_files/$file"
+    dest_file="$HOME/.${file%.*}"
+    if [ -f "$dest_file" ]; then
+      echo "Skipped (exists): $dest_file"
+    else
+      cp "$source_file" "$dest_file"
+      echo "Created: $dest_file"
+    fi
+  done
+else
+  echo "ERROR: $HOME does not exist."
+fi
+
+echo "\nDotfile install complete.\n"
