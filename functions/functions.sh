64e573bc54128c09c0c810bc8a35dc09493ad082
Brooke Kuhlmann <brooke@redalchemist.com>
2013-04-17 21:56:57 -0600

    Updated the installer to prompt for options before executing and added the ability to check for currently installed file differences.

---
+++ functions/functions.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# DESCRIPTION
+# Defines functions for installing dotfiles.
+
+# Installs a file.
+# Parameters:
+# $1 = The file name.
+function install_file {
+  source_file="home_files/$1"
+  dest_file="$HOME/.${1%.*}"
+
+  if [ -f "$dest_file" ]; then
+    echo "Exists:  $dest_file"
+  else
+    cp "$source_file" "$dest_file"
+    echo "Created: $dest_file"
+  fi
+}
+export install_file
+
+# Installs all files.
+function install_files {
+  echo "\nInstalling dotfiles...\n"
+
+  for file in `ls -1 home_files`
+  do
+    install_file $file
+  done
+
+  echo "\nDotfile install complete."
+}
+export install_files
+
+# Checks a file for changes.
+# Parameters:
+# $1 = The file name.
+function check_file {
+  source_file="home_files/$1"
+  dest_file="$HOME/.${1%.*}"
+
+  if [ "$(diff $dest_file $source_file)" == '' ]; then
+    echo "Identical: $dest_file"
+  else
+    echo "Different: $dest_file"
+  fi
+}
+export check_file
+
+# Checks all files for changes.
+function check_files {
+  echo "\nChecking dotfiles for changes...\n"
+
+  for file in `ls -1 home_files`
+  do
+    check_file $file
+  done
+
+  echo "\nDotfile check complete."
+}
+export check_files
--- install.sh
+++ install.sh
@@ -1,21 +1,41 @@
 #!/bin/sh
 
-echo "\nInstalling dotfiles...\n"
+# DESCRIPTION
+# Executes the dotfiles installer.
 
+# USAGE
+# ./install.sh
+
+# SETTINGS
+set -e # Exit if any command returns non-zero.
+
+# FUNCTIONS
+source functions/functions.sh
+
+# EXECUTION
 if [ -d "$HOME" ]; then
-  for file in `ls -1 home_files`
-  do
-    source_file="home_files/$file"
-    dest_file="$HOME/.${file%.*}"
-    if [ -f "$dest_file" ]; then
-      echo "Exists:  $dest_file"
-    else
-      cp "$source_file" "$dest_file"
-      echo "Created: $dest_file"
-    fi
+  echo ''
+
+  while true; do
+    echo "Install Options:"
+    echo "  i: Install dotfiles (existing files will be skipped)."
+    echo "  c: Check for changes since last install."
+    echo "  q: Quit/Exit."
+    echo ''
+    read -p "Enter selection: " response
+    case $response in
+      'i')
+        install_files
+        break;;
+      'c')
+        check_files
+        break;;
+      'q')
+        break;;
+    esac
   done
+
+  echo ''
 else
   echo "ERROR: $HOME does not exist."
 fi
-
-echo "\nDotfile install complete.\n"
