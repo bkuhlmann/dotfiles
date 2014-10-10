07304ea271ad47066a757a9e045e3a2b3b591a6f
Brooke Kuhlmann <brooke@redalchemist.com>
2014-10-09 20:20:53 -0600

    Refactored Git commit message prefix extension code.

--- home_files/.git_template/hooks/extensions/git.sh
+++ home_files/.git_template/hooks/extensions/git.sh
@@ -3,9 +3,10 @@
 function git_commit_message_prefix() {
   local label="[git]"
   local message_file="${BASH_ARGV[0]}"
+  local match="^(Fixed|Removed|Added|Updated|Refactored).+"
+
   read -r first_line < $message_file
 
-  local match="^(Fixed|Removed|Added|Updated|Refactored).+"
   if [[ ! "$first_line" =~ $match ]]; then
     printf "$label: Invalid commit message prefix. Use only: Fixed, Removed, Added, Updated, or Refactored.\n"
     exit 1
