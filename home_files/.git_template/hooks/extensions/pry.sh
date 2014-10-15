763ff1c50fdc24fecab1d8508e694befd0bc2c39
Brooke Kuhlmann <brooke@redalchemist.com>
2014-10-14 19:02:22 -0600

    Updated Pry Git hook to detect remote debug statements.

--- home_files/.git_template/hooks/extensions/pry.sh
+++ home_files/.git_template/hooks/extensions/pry.sh
@@ -7,10 +7,10 @@ function pry_binding() {
 
   if command -v ag > /dev/null; then
     local results=()
-    results="$(ag --file-search-regex '(rb|erb|slim)' 'binding.pry' . || true)"
+    results="$(ag --file-search-regex '(rb|erb|slim)' '(binding.pry|binding.remote_pry)' . || true)"
 
     if [[ ${#results[@]} > 0 && ${results[@]} != '' ]]; then
-      printf "$label: The following files have \"binding.pry\" statements:\n\n"
+      printf "$label: Debug statements detected:\n\n"
 
       for line in ${results[@]}; do
         printf "$line\n"
